184b14af5fc04ab9fee099a2666e9d64eed38fac
zachh12 <zhainsel@gmail.com>
2018-12-28 14:05:48 -0500

    Updated fit model

--- Fit/fit_waveform.py
+++ Fit/fit_waveform.py
@@ -8,6 +8,8 @@ import dnest4
 
 from waffle.management import WaveformFitManager
 from waffle.models import VelocityModel, LowPassFilterModel, HiPassFilterModel, ImpurityModelEnds
+from waffle.models import FirstStageFilterModel, AntialiasingFilterModel
+from waffle.models import OvershootFilterModel,OscillationFilterModel, TrappingModel
 from siggen import PPC
 
 chan_dict = {
@@ -25,7 +27,8 @@ def main(wf, doPlot=False):
     wf_idx = int(wf)
 
     chan = 626
-    directory = "chan{}_wfs".format(chan)
+    #chan = 692
+    directory = "chan{}_testwfs".format(chan)
 
     wf_file = "training_data/chan{}_5000wfs.npz".format(chan)
     conf_name = "{}.conf".format( chan_dict[chan] )
@@ -36,19 +39,35 @@ def main(wf, doPlot=False):
 
     detector = PPC( conf_file, wf_padding=100)
 
-    lp = LowPassFilterModel(detector)
-    hp = HiPassFilterModel(detector)
-    im = ImpurityModelEnds(detector)
     vm = VelocityModel(include_beta=False)
+    #lp = LowPassFilterModel(detector)
+    #hp1 = HiPassFilterModel(detector)
+    #hp2 = HiPassFilterModel(detector)
+    fs = FirstStageFilterModel(detector)
+    al = AntialiasingFilterModel(detector)
+    oshoot = OvershootFilterModel(detector)
+    osc = OscillationFilterModel(detector)
+    im = ImpurityModelEnds(detector)
+    tm = TrappingModel()
+
+    #lp = LowPassFilterModel(detector)
+    #hp = HiPassFilterModel(detector)
 
     det_params = [ 9.76373631e-01,8.35875049e-03,-5.09732644e+00,-6.00749043e+00,
                    4.74275220e+06,3.86911389e+06,6.22014783e+06,5.22077471e+06,
                     -3.63516477e+00,-4.48184667e-01]
 
-    lp.apply_to_detector(det_params[:2], detector)
-    hp.apply_to_detector(det_params[2:4], detector)
-    vm.apply_to_detector(det_params[4:8], detector)
-    im.apply_to_detector(det_params[8:], detector)
+    vm.apply_to_detector([8439025,5307015,9677126,5309391], detector)
+    #lp.apply_to_detector([9.76373631e-01, 8.35875049e-03], detector)
+    #hp1.apply_to_detector([-5.09732644e+00,-6.00749043e+00], detector)
+    #hp2.apply_to_detector([1,1], detector)
+    fs.apply_to_detector([-7.062660481537297308e-01, 9.778840228405032420e-01, -7.851819989636383390e+00], detector)
+    al.apply_to_detector([0.5344254, 0.135507736], detector)
+    oshoot.apply_to_detector([-3.8111099842, 0.140626600], detector)
+    osc.apply_to_detector([-1.6800848, 3.00040593, -1.245777055, 5.0073780147], detector)
+    im.apply_to_detector([-0.015571734108306538, -5.7326715464], detector)
+    tm.apply_to_detector(2154.3, detector)
+
 
     data = np.load(wf_file, encoding="latin1")
     wfs = data['wfs']

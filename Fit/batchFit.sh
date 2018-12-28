684fd514284e1e40d0052fc41ba2903fdef68f8b
zachh12 <zhainsel@gmail.com>
2018-12-28 13:58:38 -0500

    Set batch for single waveform at a time

--- Fit/batchFit.sh
+++ Fit/batchFit.sh
@@ -3,8 +3,7 @@ numWaveforms=6
 for ((i=0; i < $numWaveforms; ++i))
 do
     python3 fit_waveform.py $i &
-    ((++i))
-    python3 fit_waveform.py $i &
-    (sleep 10 && pkill -9 -f fit_waveform.py)
-    #python3 process.py
+    #((++i))
+    #python3 fit_waveform.py $i &
+    (checkFit.py i && pkill -9 -f fit_waveform.py)
 done

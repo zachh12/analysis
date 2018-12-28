2584756370cb7a257eb55918635d672aa44fbe02
zachh12 <zhainsel@gmail.com>
2018-12-28 13:55:52 -0500

    Checkfit working for single waveform

--- Fit/checkFit.py
+++ Fit/checkFit.py
@@ -1,16 +1,28 @@
 import dnest4 as dn4
-import os
-import sys
-def main(idx):
+import os, sys
+import time
+import numpy as np
+
+def main(idx=0):
     print(idx)
+    directory = "chan626_0-6avsewfs"
+    os.chdir(directory)
     for root, dirs, files in os.walk("."):
         for wf in dirs:
             index = "wf" + str(idx)
             if wf == index:
                 os.chdir(wf)
-                dn4.postprocess(plot=False)
-                os.chdir("..")
-        exit()
+                loop = 1
+                while loop == 1:
+                    dn4.postprocess(plot=False)
+                    post = np.loadtxt("posterior_sample.txt")
+                    if (len(post) < 10):
+                        time.sleep(150)
+                    else:
+                        os.chdir("../..")
+                        return
+            else:
+                continue
 
 if __name__ == "__main__":
     main(*sys.argv[1:2])

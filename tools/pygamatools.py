4483e474e09c296281fae7f5fd05728443f79f56
zachh12 <zhainsel@gmail.com>
2018-12-30 23:33:52 -0500

    Added plot

--- tools/pygamatools.py
+++ tools/pygamatools.py
@@ -116,15 +116,11 @@ xmin, xmax = plt.xlim()
 x = np.linspace(xmin, xmax, 300)
 p = norm.pdf(x, mu, std)
 plt.plot(x, p, 'k', linewidth=2)
-#title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
-#plt.title(title)
+title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
+plt.title(title)
 
 plt.axhline(y=(np.amax(p)/2))
-p = p - (np.amax(p)/2)
 
-minE = x[np.where(p == min(np.abs(p)))]
-maxE = x[np.where(p == max(np.abs(p)))]
-print(minE, maxE)
 print(np.std(energies)*2.35)
 plt.show()
 

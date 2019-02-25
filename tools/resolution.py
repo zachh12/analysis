781350dae3d218e337baa05bb18836a8fa0dbd12
zachh12 <zhainsel@gmail.com>
2019-02-25 16:09:24 -0500

    Removed Run3

--- tools/resolution.py
+++ tools/resolution.py
@@ -8,27 +8,61 @@ from scipy import signal
 from scipy import stats
 import os
 import h5py
-
+cal = [1173, 1332, 1460]
+adc = [211, 246, 272]
+cal = [1332, 1460, 2614]
+adc = [257, 282, 503]
+cal = [727.330, 860.564, 2614.553]
+adc = [1835.375, 2154, 6422.75]
+opt_tau = 1655.4
+#44.8
 def main():
+    df = pd.read_hdf("test_data.h5", key="data")
 
-    ecal = np.loadtxt("ecal.txt")
-    #plt.hist(ecal, bins=1300)
-    #plt.xlim(1300, 1500)
-    #plt.show()
-    #exit()
-    k40, co60 = [], []
-    for num in ecal:
-        if ((num > 1435) & (num < 1475)):
-            k40.append(num)
-        elif ((num > 1300) & (num < 1370)):
-            co60.append(num)
-        #elif ((num ))
-    print(len(k40))
-    plt.hist(k40, bins=30)
-    #plt.hist(co60)
-    print(np.std(k40)*2.35)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(adc, cal)
+    df['trap_max'] = np.float64(df['trap_max']) * slope + intercept
+    df['drift_time'] = df['drift_time'] * 10
+
+    cut =  (df['trap_max'] < 780)# & (df['trap_max'] < 2700)
+    df = df[cut]
+    print(np.std(df['trap_max']) * 2.35)
+    
+    taus = np.linspace(-1000,2000, 1000)
+    fwhms = []
+    for tau in taus:
+        fwhms.append(2.35*np.std(df['trap_max'] * np.exp(df['drift_time'] * (-tau / 10000000))))
+    tau = taus[np.argmin(fwhms)]
+
+    plt.figure(1)
+    plt.scatter(taus, fwhms)
+    print(np.min(fwhms), taus[np.argmin(fwhms)])
+    plt.figure(3)
+    plt.hist(df['trap_max'] * np.exp(df['drift_time'] * (-taus[np.argmin(fwhms)] / 10000000)), bins=40)
     plt.show()
-    #print(np.std(co60)*2.35) 
+
+def running_mean(x, N):
+    cumsum = np.cumsum(np.insert(x, 0, 0)) 
+    return (cumsum[N:] - cumsum[:-N]) / float(N)
+
+def time(df):
+    drift_time = []
+
+    for wf in df['waveform']:
+        dt = []
+        for i in range(50,len(wf)):
+
+            if wf[i] > (.9 * np.amax(wf)):
+                dt.append(i)
+                continue
+        try:
+            drift_time.append(dt[0])
+        except:
+            drift_time.append(-100)
+    
+    df['drift_time'] = drift_time - df['t0']
+    print(df['drift_time'])
+    plt.show()
+    df.to_hdf("test_data_mjd.h5", key='data')
 
 if __name__ == '__main__':
     main()
